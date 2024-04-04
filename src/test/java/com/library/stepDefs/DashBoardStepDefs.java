da7bcf7c8f6b21af4ce0358c9d7fdef0dcf3a5a0
European01 <emiryaziciofficial@gmail.com>
2024-04-03 21:59:58 -0400

    Add DashBoardStepDefs.java and LoginStepDefs.java classes.

--- src/test/java/com/library/stepDefs/DashBoardStepDefs.java
+++ src/test/java/com/library/stepDefs/DashBoardStepDefs.java
@@ -1,4 +1,24 @@
 package com.library.stepDefs;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class DashBoardStepDefs {
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String userType) {
+
+    }
+
+    @When("user gets all information from modules")
+    public void user_gets_all_information_from_modules() {
+
+    }
+
+    @Then("the information should be same with database")
+    public void the_information_should_be_same_with_database() {
+
+    }
+
 }
