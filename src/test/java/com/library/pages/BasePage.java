a7d62f1ab70942a76d7b989ac4a4f4060a075b2c
European01 <emiryaziciofficial@gmail.com>
2024-04-04 22:33:02 -0400

    Update BasePage.java class.Locate more elements and add more custom methods.

--- src/test/java/com/library/pages/BasePage.java
+++ src/test/java/com/library/pages/BasePage.java
@@ -30,15 +30,37 @@ public abstract class BasePage {
     @FindBy(xpath = "//a[.='Log Out']")
     private WebElement logOutButton;
 
+    @FindBy(xpath = "//span[@class='text-muted']//strong")
+    private WebElement libraryVersionText;
+
+    @FindBy(xpath = "//a[@class='navbar-brand']")
+    private WebElement libraryLogo;
+
     /**
      * Navigates to the specified module page.
      *
      * @param moduleName The name of the module to navigate to.
      */
     public void navigatePage(String moduleName){
+
+        BrowserUtils.waitFor(1);
         WebElement module = Driver.getDriver().findElement(
                 By.xpath("//span[@class='title'][.='"+moduleName+"']/.."));
         module.click();
+    }
+
+    /**
+     * Retrieves the text of a specific module displayed on the page.
+     *
+     * @param moduleName The name of the module to retrieve text for.
+     * @return The text of the specified module.
+     */
+    public String getModuleText(String moduleName){
+        BrowserUtils.waitFor(1);
+        WebElement module = Driver.getDriver().findElement(
+                By.xpath("//span[.='"+moduleName+"']"));
+
+        return module.getText();
     }
 
     /**
@@ -47,7 +69,7 @@ public abstract class BasePage {
      * @return The username displayed on the page.
      */
     public String getUserName(){
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(1);
         return userNameField.getText();
     }
 
@@ -55,7 +77,39 @@ public abstract class BasePage {
      * Logs out of the application.
      */
     public void logOut(){
+        BrowserUtils.waitFor(1);
         navBarDropdown.click();
+        BrowserUtils.waitFor(1);
         logOutButton.click();
+    }
+
+    /**
+     * Retrieves the version text of the library displayed on the page.
+     *
+     * @return The version text of the library.
+     */
+    public String getVersionText(){
+        BrowserUtils.waitFor(1);
+        return libraryVersionText.getText();
+    }
+
+    /**
+     * Retrieves the text of the library logo displayed on the page.
+     *
+     * @return The text of the library logo.
+     */
+    public String getLogoText(){
+        BrowserUtils.waitFor(1);
+        return libraryLogo.getText();
+    }
+
+    /**
+     * Retrieves the text of the logout button displayed on the page.
+     *
+     * @return The text of the logout button.
+     */
+    public String getLogOutButtonText(){
+        BrowserUtils.waitFor(1);
+        return logOutButton.getText();
     }
 }
